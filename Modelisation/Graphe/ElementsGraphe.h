1ad6c7dddcf0c87b2576db5e20b0f9cda15a2881
Nexey <leliam@live.fr>
2018-03-18 17:30:59 +0100

    Nettoyage de code

--- Modelisation/Graphe/ElementsGraphe.h
+++ Modelisation/Graphe/ElementsGraphe.h
@@ -8,81 +8,79 @@ using namespace std;
 
 class Sommet {
 public:
-    int degre;
-    int inf;
-    int sup;
+	int degre;
+	int inf;
+	int sup;
 
-    string nom;
-    //info = valuation du sommet : poids arete ascendante + valuation sommmet pere
-    Sommet* pere;
+	string nom;
+	//info = valuation du sommet : poids arete ascendante + valuation sommmet pere
+	Sommet* pere;
 
-    Sommet(const string& nom, int borneInf, int borneSup) : degre(0), nom(nom), inf(borneInf), sup(borneSup){}
+	Sommet(const string& nom, int borneInf, int borneSup) : degre(0), nom(nom), inf(borneInf), sup(borneSup) {}
 
-    operator string () const;
+	operator string () const;
 
 };
 
 
 class Arc {
 public:
-    Sommet *debut, *fin;
+	Sommet * debut, *fin;
 
-    int cout;
-    int temps;
+	int cout;
+	int temps;
 
-    string nom;
+	string nom;
 
-    Arc(const string& nom, int cout, int temps, Sommet * debut, Sommet * fin) : nom(nom), cout(cout), temps(temps), debut(debut), fin(fin) {
-            debut->degre++;
-            fin->degre++;
-            debut->pere = fin;
-    }
+	Arc(const string& nom, int cout, int temps, Sommet * debut, Sommet * fin) : nom(nom), cout(cout), temps(temps), debut(debut), fin(fin) {
+		debut->degre++;
+		fin->degre++;
+		debut->pere = fin;
+	}
 
-    ~Arc() { debut->degre--; fin->degre--; }
+	~Arc() { debut->degre--; fin->degre--; }
 
-    operator string () const;
+	operator string () const;
 
-    bool estEgal(const Sommet * debut, const Sommet * fin) const;
+	bool estEgal(const Sommet * debut, const Sommet * fin) const;
 };
 
 
 //#########SOMME
 Sommet::operator string () const {
-        ostringstream oss;
-        oss << "Sommet[";
-        oss << "nom:" << nom << ";";
-        oss <<  "degre:" << degre << ";";
-        oss << "inf:" << inf << ";";
-        oss << "sup:" << sup;
-        oss <<  "]";
-        return oss.str();
+	ostringstream oss;
+	oss << "Sommet[";
+	oss << "nom:" << nom << ";";
+	oss << "degre:" << degre << ";";
+	oss << "inf:" << inf << ";";
+	oss << "sup:" << sup;
+	oss << "]";
+	return oss.str();
 }
 
 ostream & operator << (ostream & os, const Sommet & sommet) {
-        return os << (string)sommet;
+	return os << (string)sommet;
 }
 
 //#########ARC
 Arc::operator string () const {
-        ostringstream oss;
-        oss << "Arc[";
-        oss << "nom:" << nom << ";   ";
-        oss << "cout:" << cout << ";   ";
-        oss << "temps:" << temps << ";   ";
-        oss << "debut:" << debut->nom << ";   ";
-        oss << "fin:" << fin->nom;
-        oss << "]";
-        return oss.str();
+	ostringstream oss;
+	oss << "Arc[";
+	oss << "nom:" << nom << ";   ";
+	oss << "cout:" << cout << ";   ";
+	oss << "temps:" << temps << ";   ";
+	oss << "debut:" << debut->nom << ";   ";
+	oss << "fin:" << fin->nom;
+	oss << "]";
+	return oss.str();
 
 }
 
 ostream & operator << (ostream & os, const Arc& arc) {
-        return os << (string)arc;
+	return os << (string)arc;
 }
 
 bool Arc::estEgal(const Sommet * deb, const Sommet * fi) const {
-        // Attention : Nous sommes dans un graphe orienté, et donc s1 doit être le début et s2 la fin
-        return deb == debut && fi == fin;
+	// Attention : Nous sommes dans un graphe orienté, et donc s1 doit être le début et s2 la fin
+	return deb == debut && fi == fin;
 }
-
-
