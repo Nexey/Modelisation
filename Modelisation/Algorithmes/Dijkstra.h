ff9ab76260cea3c8473b7b8f8749de51ad5d681a
Nexey <leliam@live.fr>
2018-03-19 14:19:38 +0100

    Rajout d'outils

--- Modelisation/Algorithmes/Dijkstra.h
+++ Modelisation/Algorithmes/Dijkstra.h
@@ -5,15 +5,7 @@
 #pragma once
 
 #include "../Graphe/Graphe.h"
-
-bool aucuneValuationNegative(Maillon<Arc> *lArc) {
-    if (lArc != NULL) {
-        if (lArc->valeur->coutArc < 0 || lArc->valeur->tempsArc < 0) return false;
-        return aucuneValuationNegative(lArc->suivant);
-    }
-    // On est arrive a la fin de la liste des arcs, donc on peut renvoyer true
-    return true;
-}
+#include "../Outils/Outils.h"
 
 int coutArc(const Arc* a) {
 	return a->coutArc;
--- Modelisation/Main/Main.cpp
+++ Modelisation/Main/Main.cpp
@@ -7,9 +7,10 @@ using namespace std;
 
 int main() {
 	//string chemin = "./Data/circuit.txt";
-
 	//string chemin = "./Data/data_VRPTW_160_10_5_10.gpr";
 	string chemin = "./Data/data_VRPTW_10.gpr";
+
+
 
 	ChargerGraphe infosGraphe(chemin);
 
@@ -20,7 +21,7 @@ int main() {
 
 	cout << (aucuneValuationNegative(graphe->lArcs) ? "Il n'y a pas de valuations negatives." : "Il existe au moins une valuation negative.") << endl;
 
-	cout << dijkstra(graphe, coutArc) << endl;
+	cout << dijkstra(graphe, coutArc) << endl << endl;
 	cout << dijkstra(graphe, tempsArc) << endl;
 	system("pause");
 }
