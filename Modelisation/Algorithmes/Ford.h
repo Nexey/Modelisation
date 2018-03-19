c2cb7076a3de199fc43ca76a70b1b3c1120f4ec7
Charlotte LANUEL <charlotte.lanuel@hotmail.fr>
2018-03-19 17:01:40 +0100

    suite ford

--- Modelisation/Algorithmes/Ford.h
+++ Modelisation/Algorithmes/Ford.h
@@ -3,12 +3,16 @@
 //
 #include "../Graphe/Graphe.h"
 #include "../Algorithmes/ParcoursDFS.h"
+#include "../Outils/Outils.h"
 
 
 #ifndef MODELISATION_FORD_H
 #define MODELISATION_FORD_H
 
 bool detection_circuit_negatif(const Graphe *g, Sommet *depart, Maillon<Sommet>* SommetsMarquesParent){
+    if(aucuneValuationNegative(g->lArcs))
+        return false;
+
     for(Maillon<Sommet>*l = g->lSommets; l; l->suivant){
         Maillon<Sommet>* voisins = NULL;
         Maillon<Sommet>* SommetsMarques = NULL;
@@ -22,7 +26,7 @@ bool detection_circuit_negatif(const Graphe *g, Sommet *depart, Maillon<Sommet>*
             //Evite un appel récursif inutile (un simple return true) pour eviter les stack overflow sur les gros graphes
             if(Maillon<Sommet>::appartient(voisins->valeur, SommetsMarquesParent)){
                 for(Maillon<Arc> *m = g->lArcs; m; m->suivant){
-                    if(m->valeur->fin->equals(voisins->valeur) && (m->valeur->cout < 0))
+                    if(m->valeur->fin->equals(voisins->valeur) && (m->valeur->coutArc < 0))
                         return true;
                 }
 
@@ -36,7 +40,9 @@ bool detection_circuit_negatif(const Graphe *g, Sommet *depart, Maillon<Sommet>*
 }
 
 
-Maillon<Arc> Ford(const Graphe *g, Sommet *debut, int(*etiquette)(const ))
+Maillon<Arc> Ford(const Graphe *g, Sommet *debut, int(*etiquette)(const Arc*)){
+    if(!detection_circuit_negatif((g, debut, )))
+}
 
 
 #endif //MODELISATION_FORD_H
