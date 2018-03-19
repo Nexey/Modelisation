585e1c2e5d0beb716e246733b92be2edce99473a
Nexey <leliam@live.fr>
2018-03-19 21:48:12 +0100

    Suppression de max

--- Modelisation/Graphe/Maillon.h
+++ Modelisation/Graphe/Maillon.h
@@ -8,9 +8,6 @@
 using namespace std;
 
 template<class Type>
-Type max(const Type &a, const Type &b) { return a > b ? a : b; }
-
-template<class Type>
 Type minimum(const Type &a, const Type &b) { return a < b ? a : b; }
 
 template <class Type>
