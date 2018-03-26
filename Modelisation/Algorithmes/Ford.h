e15a36414c8ad205c7f14e80abaf0cd5a3172b68
Nexey <leliam@live.fr>
2018-03-26 08:56:06 +0200

    Projet aussi fini que possible

--- Modelisation/Algorithmes/Ford.h
+++ Modelisation/Algorithmes/Ford.h
@@ -3,84 +3,84 @@
 //
 #pragma once
 
-
 #include "../Graphe/Graphe.h"
 #include "../Algorithmes/ParcoursDFS.h"
 #include "../Outils/Outils.h"
 
-int avec_cout_Arc(Arc* a){return a->coutArc;}
+int avec_cout_Arc(Arc* a) { return a->coutArc; }
 
-int avec_temps_Arc(Arc* a){return a->tempsArc;}
+int avec_temps_Arc(Arc* a) { return a->tempsArc; }
 
 
-bool detection_circuit_negatif(Graphe *g, Sommet *depart,int(*valuation)(Arc*)){
-    Maillon<Maillon<Arc>>* circuits = recherche_Circuit(g, depart);
-    int val;
+bool detection_circuit_negatif(Graphe *g, Sommet *depart, int(*valuation)(Arc*)) {
+	Maillon<Maillon<Arc>>* circuits = recherche_Circuit(g, depart);
+	int val;
 
-    for(; circuits; circuits = circuits->suivant){
+	for (; circuits; circuits = circuits->suivant) {
 		val = 0;
-        for(; circuits->valeur; circuits->valeur = circuits->valeur->suivant){
+		for (; circuits->valeur; circuits->valeur = circuits->valeur->suivant) {
 			val += valuation(circuits->valeur->valeur);
-        }
-        if(valuation < 0) return true;
+		}
+		if (valuation < 0) return true;
 
-    }
-    return false;
+	}
+	return false;
 }
 
 
-Maillon<Arc> * Ford(Graphe *g, Sommet *debut, int(*valuation)(Arc*)= &avec_cout_Arc){
-   // if(!detection_circuit_negatif(g, debut, valuation))	std::cerr << "L'algorithme n'est pas applicable car un circuit negatif est present" << endl;
+Maillon<Arc> * Ford(Graphe *g, Sommet *debut, int(*valuation)(Arc*) = &avec_cout_Arc) {
+	if (detection_circuit_negatif(g, debut, valuation))
+		std::cerr << "L'algorithme n'est pas applicable car un circuit negatif est present" << endl;
 
 	Maillon<Sommet> *lTmp = g->lSommets;
 	Sommet *fin = NULL;
 	for (; lTmp; lTmp = lTmp->suivant)
 		fin = lTmp->valeur;
 
-    Maillon<Sommet>* sommetATraiter = g->lSommets;
-    Maillon<Sommet>* prochainSommetATraiter = new Maillon<Sommet>(debut, nullptr);
-    Maillon<pair<Sommet*,Arc*>>* adj;
+	Maillon<Sommet>* sommetATraiter = g->lSommets;
+	Maillon<Sommet>* prochainSommetATraiter = new Maillon<Sommet>(debut, nullptr);
+	Maillon<pair<Sommet*, Arc*>>* adj;
 
-    for(; sommetATraiter; sommetATraiter = sommetATraiter->suivant)
-        sommetATraiter->valeur->etiquette = INT8_MAX;
+	for (; sommetATraiter; sommetATraiter = sommetATraiter->suivant)
+		sommetATraiter->valeur->etiquette = INT8_MAX;
 
-    debut->etiquette = 0;
+	debut->etiquette = 0;
 
-    do{
-        sommetATraiter = prochainSommetATraiter;
-        prochainSommetATraiter = nullptr;
+	do {
+		sommetATraiter = prochainSommetATraiter;
+		prochainSommetATraiter = nullptr;
 
-        for(; sommetATraiter; sommetATraiter = sommetATraiter->suivant) {
-            adj = g->adjacences(sommetATraiter->valeur);
-            for (; adj; adj = adj->suivant) {
-                int coutSommetPlusArc = adj->valeur->first->etiquette + valuation(adj->valeur->second);
-                int coutAModifier = adj->valeur->second->fin->etiquette;
-                if (coutSommetPlusArc < coutAModifier) {
-                    adj->valeur->second->fin->etiquette = coutSommetPlusArc;
-                    prochainSommetATraiter = new Maillon<Sommet>(adj->valeur->second->fin, prochainSommetATraiter);
-                }
-            }
-        }
+		for (; sommetATraiter; sommetATraiter = sommetATraiter->suivant) {
+			adj = g->adjacences(sommetATraiter->valeur);
+			for (; adj; adj = adj->suivant) {
+				int coutSommetPlusArc = adj->valeur->first->etiquette + valuation(adj->valeur->second);
+				int coutAModifier = adj->valeur->second->fin->etiquette;
+				if (coutSommetPlusArc < coutAModifier) {
+					adj->valeur->second->fin->etiquette = coutSommetPlusArc;
+					prochainSommetATraiter = new Maillon<Sommet>(adj->valeur->second->fin, prochainSommetATraiter);
+				}
+			}
+		}
 
 	} while (prochainSommetATraiter != nullptr);
 
 
-    Maillon<Arc>* chemin = nullptr;
-    bool continuer = true;
-    Sommet* actuel = debut;
-    Arc* arcActuel = nullptr;
-    int min;
+	Maillon<Arc>* chemin = nullptr;
+	bool continuer = true;
+	Sommet* actuel = debut;
+	Arc* arcActuel = nullptr;
+	int min;
 
 
-    while (actuel != fin){
-        adj = g->adjacences(actuel);
-        min = INT8_MAX;
-        for (; adj; adj = adj->suivant) {
+	while (actuel != fin) {
+		adj = g->adjacences(actuel);
+		min = INT8_MAX;
+		for (; adj; adj = adj->suivant) {
 			if (adj->valeur->first->etiquette < min) {
-                min = adj->valeur->first->etiquette;
-            }
+				min = adj->valeur->first->etiquette;
+			}
 			arcActuel = adj->valeur->second;
-        }
+		}
 
 		if (arcActuel) {
 			chemin = new Maillon<Arc>(arcActuel, chemin);
@@ -88,8 +88,7 @@ Maillon<Arc> * Ford(Graphe *g, Sommet *debut, int(*valuation)(Arc*)= &avec_cout_
 		}
 	}
 
-    return chemin;
+	return chemin;
 
 
 }
-
--- Modelisation/Main/Main.cpp
+++ Modelisation/Main/Main.cpp
@@ -9,21 +9,23 @@ int main(int argc, char *argv[]) {
 	ChargerGraphe infosGraphe(detectionChemin(argc, argv));
 
 	Graphe *graphe = infosGraphe.creationGraphe();
-	/*
+	
 	cout << endl << *graphe << endl;
 	
 	cout << "Algorithme de detection de circuit :" << endl;
 	system("pause");
-	cout << (detection_De_Circuit(graphe, graphe->lSommets->valeur) ? "Il existe un circuit." : "Il n'existe aucun circuit.") << endl;
+	cout << endl << (detection_De_Circuit(graphe, graphe->lSommets->valeur) ? "Il existe un circuit." : "Il n'existe aucun circuit.") << endl;
 
-	cout << endl << "Algorithme de Dijkstra avec le cout :";
+	cout << endl << "Algorithme de Dijkstra avec le cout :" << endl;
 	system("pause");
 	cout << endl << dijkstra(graphe, coutArc) << endl << endl;
 
-	cout << endl << "Algorithme de Dijkstra avec le temps :";
+	cout << endl << "Algorithme de Dijkstra avec le temps :" << endl;
 	system("pause");
-	cout << endl << dijkstra(graphe, tempsArc) << endl;
-	*/
+	cout << endl << dijkstra(graphe, tempsArc) << endl << endl;
+	
+	cout << "Algorithme de Ford avec le cout :" << endl;
+	system("pause");
 	cout << endl << Ford(graphe, graphe->lSommets->valeur);
 
 	cout << endl << endl << "Fin du programme." << endl;
